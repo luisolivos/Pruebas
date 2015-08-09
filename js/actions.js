cbdf95698e4201cd47e1f6abcbb4bbf0c02b9090
luisolivos <pp_its@hotmail.com>
2015-08-09 09:50:49 -0500

    jh
    j

--- js/actions.js
+++ js/actions.js
@@ -5,11 +5,16 @@ var fn =
 	},
 	
 	conectarAjax: function(){
+        alert();
+        
 		$.ajax({
-			method: 'POST',
+			type: 'POST',
             url:'http://192.168.1.129/Default.aspx/OnSubmit',
 			//url: 'http://carlos.igitsoft.com/apps/test.php',
-			data: {mail: 'Nom'},
+			data: '{mail: "Nom"}',
+            contentType: 'application/json: charset=utf-8',
+            dataType:'json',
+            success: function(){alert('success');},
 			error: function(){alert('ajax connection failed');}
 		}).done(function(msg){alert ('correct');});
 	},
